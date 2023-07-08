7aac1b137cbffa197d482f44f595a10535482ae6
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-08 20:13:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,19 +9,19 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "4500"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://graph.org/file/6e5004198c3bdbba84bdc.jpg")
-START_IMG = getenv("START_IMG", "https://graph.org/file/6e5004198c3bdbba84bdc.jpg")
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/4d4eca0ac2202c09cd2e8.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/e776e115ba29389aa305f.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/EE_47")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/EE_20")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ah07v")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ah07v")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5656828413").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1748768168").split()))
 
 
-FAILED = "https://graph.org/file/6e5004198c3bdbba84bdc.jpg"
+FAILED = "https://te.legra.ph/file/e776e115ba29389aa305f.jpg"
